af3f00e8bb092205fc5d95c84e31f01827d746bf
Ricardo Ordoñez <yoricardoxd@gmail.com>
2017-09-15 12:11:14 -0400

    'newman'

--- server.js
+++ server.js
@@ -1,105 +1,107 @@
-//  OpenShift sample Node application
-var express = require('express'),
-    app     = express(),
-    morgan  = require('morgan');
-    
-Object.assign=require('object-assign')
-
-app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'))
-
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
+var path = require('path'),
+  async = require('async'), //https://www.npmjs.com/package/async
+  newman = require('newman'),
+  fs = require('fs');
 
-if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
-  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
-      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
-      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
-      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
-      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
-      mongoUser = process.env[mongoServiceName + '_USER'];
-
-  if (mongoHost && mongoPort && mongoDatabase) {
-    mongoURLLabel = mongoURL = 'mongodb://';
-    if (mongoUser && mongoPassword) {
-      mongoURL += mongoUser + ':' + mongoPassword + '@';
-    }
-    // Provide UI label that excludes user id and pw
-    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
-
-  }
-}
-var db = null,
-    dbDetails = new Object();
-
-var initDb = function(callback) {
-  if (mongoURL == null) return;
-
-  var mongodb = require('mongodb');
-  if (mongodb == null) return;
-
-  mongodb.connect(mongoURL, function(err, conn) {
-    if (err) {
-      callback(err);
-      return;
-    }
-
-    db = conn;
-    dbDetails.databaseName = db.databaseName;
-    dbDetails.url = mongoURLLabel;
-    dbDetails.type = 'MongoDB';
-
-    console.log('Connected to MongoDB at: %s', mongoURL);
-  });
+var parametersForTestRun = {
+    collection: path.join(__dirname, 'chat.postman_collection.json'), // your collection
+    environment: path.join(__dirname, 'test.postman_environment.json'), //your env
+  };
+var http = require('http');
+parallelCollectionRun = function(done) {
+  newman.run(parametersForTestRun, done);
 };
+var tests=[];
 
-app.get('/', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    var col = db.collection('counts');
-    // Create a document with request IP and current time of request
-    col.insert({ip: req.ip, date: Date.now()});
-    col.count(function(err, count){
-      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
+
+
+
+
+var runTest = function (res) {
+    console.log('run');
+    // Runs the Postman sample collection thrice, in parallel.
+async.parallel(tests,
+  function(err, results) {
+    err && console.error(err);
+
+    results.forEach(function(result) {
+      var failures = result.run.failures;
+      console.info(failures.length ? JSON.stringify(failures.failures, null, 2) :
+             
+        `${result.collection.name} ran successfully.`);
+//console.log(result);
+
     });
-  } else {
-    res.render('index.html', { pageCountMessage : null});
-  }
-});
+    res.write('Runed');
+    res.end();
+    tests=[];
+  });
+  
+  
+}
+var loadTest = function (max) {
+    for (var i = 0; i < max; i++) {
+        tests.push(parallelCollectionRun);
+        
+        if(i>=max){
+            runTest();
+        }
+    }
+    
+}
+http.createServer(function (req, res) {
+    //console.log(req);
+    console.log(req.url);
+    console.log(req.method);
+    var re = /^((\/\w+)*\/)([\w\-\.]+[^#?\s]+)(.*)?(#[\w\-]+)?$/;
+    var test = re.exec(req.url);
+    //test[2].replace("/","");
+    var number="";
+    if(test!=null){
+        try{
+            number = test[2].replace("/","");
+            number = parseInt(number);
+            if(number>100){
+                throw new Error("Limit concurrents");
+            }
+            if(test[3]=='dudnd73nbx823nbxins8jxznd83ndinxomsjdnew8723ndushg237wndjsd73slks83'
+                &&req.method=='GET'){
 
-app.get('/pagecount', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    db.collection('counts').count(function(err, count ){
-      res.send('{ pageCount: ' + count + '}');
-    });
-  } else {
-    res.send('{ pageCount: -1 }');
-  }
-});
+                loadTest(number);
+                //res.write('run..!');
+                runTest(res);
+               // res.write('Service active');
+               // res.end();
+                return;
+            }else{
+                throw new Error("method error");
+            }
+        }catch (exception) {
+            res.write('Service active:'+exception);
+            res.end();
+            return;
+        }
+    }else{
+       fs.readFile('./views/index.html', function (err, html) {
+            if (err) {
+                console.log(err); 
+            }       
+                res.writeHeader(200, {"Content-Type": "text/html"});  
+                res.write(html);  
+                res.end(); 
+        });  
+    }
 
-// error handling
-app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.status(500).send('Something bad happened!');
-});
-
-initDb(function(err){
-  console.log('Error connecting to Mongo. Message:\n'+err);
-});
-
-app.listen(port, ip);
-console.log('Server running on http://%s:%s', ip, port);
-
-module.exports = app ;
+    
+    
+   
+    
+  //res.writeHead(200, {'Content-Type': 'text/plain'});
+    
+ // res.write('Service active');
+  //res.end();
+}).listen(port);
